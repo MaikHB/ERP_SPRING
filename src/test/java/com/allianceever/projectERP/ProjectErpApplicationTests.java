4d5771a799a0d370b5482eddf2b55bdfd1de23ff
MaikHB <michaelhuaman732@gmail.com>
2025-06-24 22:03:11 -0300

    carpetas nuevas para q funcione el ci

--- src/test/java/com/allianceever/projectERP/ProjectErpApplicationTests.java
+++ src/test/java/com/allianceever/projectERP/ProjectErpApplicationTests.java
@@ -2,8 +2,10 @@ package com.allianceever.projectERP;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjectErpApplicationTests {
 
 	@Test
